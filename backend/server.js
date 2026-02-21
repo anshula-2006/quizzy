fdede483b6b2d411a3224cdfb25c807acbefc536
Anshula <anshulandal@gmail.com>
2026-02-21 13:19:20 +0530

    Rewritten server.js

--- backend/server.js
+++ backend/server.js
@@ -99,6 +99,10 @@ try {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
